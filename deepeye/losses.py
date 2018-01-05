8412b6e994b437368cd16c119ebf5a1e7e32116f
lpcinelli <lpcinelli@gmail.com>
2018-01-05 20:10:32 -0200

    Fix BCE and smooth Dice
    BCE mean should be computed from valid pixels (ROI) only
    Adding optional smooth value for the Dice loss

--- deepeye/losses.py
+++ deepeye/losses.py
@@ -7,23 +7,23 @@ from torch.nn.modules.module import Module
 
 class MaskedBinaryCrossEntropy(Module):
     def forward(self, input, target, roi=roi):
-        return torch.mean(
-            F.binary_cross_entropy(
-                torch.sigmoid(input), target, size_average=False) * roi)
+        return F.binary_cross_entropy(
+               torch.sigmoid(input), target, size_average=False) * roi).sum()/roi.sum()
 
 
 bce = MaskedBinaryCrossEntropy
 
 
 class MaskedDiceLoss(Module):
-    def __init__(self, th, eps=1e-12):
+    def __init__(self, th= 0.5, smooth=1, eps=1e-12):
         super().__init__()
         self.th = th
         self.eps = eps
+        self.smooth = smooth
 
     def forward(self, input, target, roi=roi):
         input = (torch.sigmoid(input) > self.th).float()
         intersect = (input * target * roi).view(input.size(0), -1).sum(1)
         union = ((input + target) * roi).view(input.size(0), -1).sum(1)
 
-        return (1 - 2 * intersect / (union + self.eps)).mean()
+        return (1 - (2 * intersect + self.smooth) / ((union + self.eps)).mean() + self.smooth)

9b26ee6f6cb84fe1fa9833fe6c7258f9f4a39e78
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2018-01-07 20:29:10 -0200

    Added all defined metrics

--- deepeye/metrics.py
+++ deepeye/metrics.py
@@ -1,63 +1,150 @@
-from sklearn import metrics
+""" Those metrics are restricted to binary classification task
 
+TODO
+    Add docstring for each method
+"""
 import torch
-import numpy as np
 
 
-def acc_score():
-    pass
+def _sanitize(y_true, y_pred, roi):
+    if type(y_pred) == torch.autograd.Variable:
+        y_pred = y_pred.data
+
+    # Flatten
+    y_pred = y_pred.view(y_pred.shape[0], -1)
+
+    if type(y_pred) != torch.ByteTensor:
+        raise ValueError('y_pred must be a ByteTensor, got {}'.format(
+            type(y_pred)))
+
+    if type(y_true) == torch.autograd.Variable:
+        y_true = y_true.data
+
+    # Flatten
+    y_true = y_true.view(y_true.shape[0], -1)
+
+    if type(y_true) != torch.ByteTensor:
+        raise ValueError('y_true must be a ByteTensor, got {}'.format(
+            type(y_true)))
+
+    if roi is None:
+        roi = torch.ones_like(y_pred)
+
+    return y_true, y_pred, roi
 
 
-def prec_score():
-    pass
+def _tn(y_true, y_pred, roi):
+    return torch.sum((((y_pred == 0) & (y_true == 0)) * roi).float(), dim=-1)
 
 
-def recall_score():
-    pass
+def _tp(y_true, y_pred, roi):
+    return torch.sum((((y_pred == 1) & (y_true == 1)) * roi).float(), dim=-1)
 
 
-def f1_score(y_pred, y_true, threshold=0.5):
+def _fp(y_true, y_pred, roi):
+    return torch.sum((((y_pred == 1) & (y_true == 0)) * roi).float(), dim=-1)
+
+
+def _fn(y_true, y_pred, roi):
+    return torch.sum((((y_pred == 0) & (y_true == 1)) * roi).float(), dim=-1)
+
+
+def acc_score(y_true, y_pred, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    tn = _tn(y_true, y_pred, roi)
+    tp = _tp(y_true, y_pred, roi)
+
+    return torch.mean((tp + tn) / (roi.float().sum(dim=-1) + eps))
+
+
+def prec_score(y_true, y_pred, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    tp = _tp(y_true, y_pred, roi)
+    fp = _fp(y_true, y_pred, roi)
+
+    return torch.mean(tp / (tp + fp + eps))
+
+
+def recall_score(y_true, y_pred, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    tp = _tp(y_true, y_pred, roi)
+    fn = _fn(y_true, y_pred, roi)
+
+    return torch.mean(tp / (tp + fn + eps))
+
+
+def f1_score(y_true, y_pred, roi=None, eps=1e-12):
     # Dice's index : 2*TP/(2*TP + FP + FN)
-    return fbeta_score(y_pred, y_true, 1, threshold)
+    return fbeta_score(y_true, y_pred, 1, roi, eps)
 
 
-def fbeta_score(y_pred, y_true, roi=None, beta=1, threshold=0.5, eps=1e-12):
-    roi = roi or torch.ones_like(y_pred)
+def fbeta_score(y_true, y_pred, beta, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
     beta2 = beta**2
 
-    y_pred = (torch.sigmoid(y_pred.float()) > threshold).float() * roi
-    y_true = y_true.float() * roi
+    tp = _tp(y_true, y_pred, roi)
+    fp = _fp(y_true, y_pred, roi)
+    fn = _fn(y_true, y_pred, roi)
 
-    true_positive = (y_pred * y_true).sum(dim=1)
-    precision = true_positive / (y_pred.sum(dim=1) + eps)
-    recall = true_positive / (y_true.sum(dim=1) + eps)
-
-    return torch.sum((1 + beta2) * (precision * recall) /
-                     (precision * beta2 + recall + eps)) / roi.sum()
+    return torch.mean(
+        (1 + beta2) * tp / ((1 + beta2) * tp + beta2 * fn + fp + eps))
 
 
-def false_pos_rate():
-    pass
+def false_pos_rate(y_true, y_pred, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    fp = _fp(y_true, y_pred, roi)
+    tn = _tn(y_true, y_pred, roi)
+
+    return torch.mean(fp / (fp + tn + eps))
 
 
-def false_neg_rate():
-    pass
+def false_neg_rate(y_true, y_pred, roi=None, eps=1e-12):
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    fn = _fn(y_true, y_pred, roi)
+    tp = _tp(y_true, y_pred, roi)
+
+    return torch.mean(fn / (fn + tp + eps))
 
 
-def true_pos_rate():
-    return recall_score()
+def true_pos_rate(y_true, y_pred, roi=None, eps=1e-12):
+    # or sensitivity
+    return 1 - false_neg_rate(y_true, y_pred, roi, eps)
 
 
-def true_neg_rate():
-    pass
+def true_neg_rate(y_true, y_pred, roi=None, eps=1e-12):
+    # or specificity
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    fp = _fp(y_true, y_pred, roi)
+    tn = _tn(y_true, y_pred, roi)
+
+    return torch.mean(tn / (fp + tn + eps))
 
 
-def IoU_score():
+def IoU_score(y_true, y_pred, roi=None, eps=1e-12):
     # Jaccard's index : TP/(TP + FP + FN)
-    pass
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    tp = _tp(y_true, y_pred, roi)
+    fp = _fp(y_true, y_pred, roi)
+    fn = _fn(y_true, y_pred, roi)
+
+    return torch.mean(tp / (tp + fp + fn + eps))
 
 
-def total_error():
+def total_error(y_true, y_pred, roi=None, eps=1e-12):
     # Total error: (FN + FP)/(TP + FP + TN + FN)
     # https://stats.stackexchange.com/questions/273537/f1-dice-score-vs-iou
-    pass
+    y_true, y_pred, roi = _sanitize(y_true, y_pred, roi)
+
+    tp = _tp(y_true, y_pred, roi)
+    tn = _tn(y_true, y_pred, roi)
+    fp = _fp(y_true, y_pred, roi)
+    fn = _fn(y_true, y_pred, roi)
+
+    return torch.mean((fn + fp) / (tp + fp + tn + fn + eps))

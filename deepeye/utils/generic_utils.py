a640eadc74ac7172f1c96721ab9b050887e2692e
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2018-01-10 16:27:41 -0200

    Fix PEP8 style Add dilation in compute_output_size

--- deepeye/utils/generic_utils.py
+++ deepeye/utils/generic_utils.py
@@ -1,7 +1,9 @@
+import numpy as np
 
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
+
     def __init__(self):
         self.reset()
 
@@ -20,41 +22,53 @@ class AverageMeter(object):
 
 class History(AverageMeter):
     """ Stores values and computes some metrics """
+
     def reset(self):
-        super(self, AverageMeter).reset()
+        super().reset()
         self.vals = []
 
     def update(self, val, n=1):
-        super(self, AverageMeter).update(val, n)
+        super().update(val, n)
         self.vals.append(val * n)
 
-def compute_pad_size(sizein, sizewin, stride):
-    """ Computes the necessary amount of padding to each side border 
+
+def compute_pad_size(input_size, kernel_size, stride=1, dilation=1):
+    """ Computes the necessary amount of padding to each side border
         of the output so that an integer number of kernels fit in
         the input
-        Inputs: 
-                sizein: tuple with height h and width w of the input tensor
-                sizewin: tuple with  kernel dimensions
+        Inputs:
+                input_size: tuple with height h and width w of the input tensor
+                kernel_size: tuple with  kernel dimensions
                 stride: tuple with kernel's stride along each dim
         Returns:
                 (vertical pad, horizontal pad)
     """
-    return (int((stride[0] - (sizein[0]-sizewin[0])%stride[0])/2.0 + 0.5),
-            int((stride[1] - (sizein[1]-sizewin[1])%stride[1])/2.0 + 0.5))
+    return (int((stride[0] -
+                 (input_size[0] - kernel_size[0]) % stride[0]) / 2.0 + 0.5),
+            int((stride[1] -
+                 (input_size[1] - kernel_size[1]) % stride[1]) / 2.0 + 0.5))
 
-def compute_output_size(sizein, sizewin, stride, pad):
-    """ Computes height and width size of the output tensor 
-        Inputs: 
-                sizein: tuple with height h and width w of the input tensor
-                sizewin: tuple with  kernel dimensions
+
+def compute_output_size(input_size,
+                        kernel_size,
+                        stride=1,
+                        padding=0,
+                        dilation=1):
+    """ Computes height and width size of the output tensor
+        Inputs:
+                input_size: tuple with height h and width w of the input tensor
+                kernel_size: tuple with  kernel dimensions
                 stride: tuple with kernel's stride along each dim
-                pad: tuple with the (half the)  amount of padding on each dim
+                padding: tuple with the (half the)  amount of padding on each dim
         Returns:
                 (output height, output width)
 
     """
-    return (int((sizein[0] + 2*pad[0] - sizewin[0])/2) + 1,
-            int((sizein[1] + 2*pad[1] - sizewin[1])/2) + 1)
+    return (np.floor((input_size[0] + 2 * padding[0] - dilation[0] *
+                      (kernel_size[0] - 1) - 1) / stride[0] + 1),
+            np.floor((input_size[1] + 2 * padding[1] - dilation[1] *
+                      (kernel_size[1] - 1) - 1) / stride[1] + 1))
+
 
 def find_threshold(y_pred, y_true, metric, min_val=0, max_val=1.0, eps=1e-6):
     '''
@@ -71,17 +85,17 @@ def find_threshold(y_pred, y_true, metric, min_val=0, max_val=1.0, eps=1e-6):
     '''
 
     # Set initial values
-    thrs_cur = (min_val+max_val)/2.0
+    thrs_cur = (min_val + max_val) / 2.0
     thrs_low = min_val
     thrs_hgh = max_val
 
     # Bisection algorithm
-    while(True):
+    while (True):
 
         # Initializing bisection algorithm
-        thrs_cur = (min_val+max_val)/2.0
-        thrs_low = (thrs_cur+min_val)/2.0
-        thrs_hgh = (thrs_cur+max_val)/2.0
+        thrs_cur = (min_val + max_val) / 2.0
+        thrs_low = (thrs_cur + min_val) / 2.0
+        thrs_hgh = (thrs_cur + max_val) / 2.0
 
         # Scores
         scr_low = metric(y_pred, y_true, thrs_low)
@@ -95,7 +109,7 @@ def find_threshold(y_pred, y_true, metric, min_val=0, max_val=1.0, eps=1e-6):
             min_val = thrs_low
 
         # End condition
-        if (abs(max_val-min_val)<=eps):
+        if (abs(max_val - min_val) <= eps):
             break
 
     # Return

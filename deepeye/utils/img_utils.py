90b11259a7a7f3f3872cbb644918c21764d347de
lpcinelli <lpcinelli@gmail.com>
2018-01-29 16:14:36 -0200

    Removed unused function
    Removed default loader extension for TIF files

--- deepeye/utils/img_utils.py
+++ deepeye/utils/img_utils.py
@@ -1,12 +1,9 @@
-from PIL import ImageFile, Image  # To solve load problems
+from PIL import ImageFile  # To solve load problems
 from skimage import io
-
-from torchvision.datasets.folder import default_loader as torch_default_loader
 
 # Valid images extensions
 IMG_EXTENSIONS = [
-    '.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm', '.PPM', '.bmp',
-    '.BMP', '.tif', '.TIF'
+    '.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm'
 ]
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -21,14 +18,3 @@ def is_image_file(filename):
     '''
     return filename.endswith(tuple(IMG_EXTENSIONS))
 
-
-def default_loader(path):
-    try:
-        return torch_default_loader(path)
-    except OSError:
-        import numpy as np
-        if path.endswith(tuple(['.tif', '.TIF'])):
-            data = (io.imread(path).astype(np.float32) / (2**8 + 1)).astype(
-                np.uint8)
-            return Image.fromarray(data)
-        raise

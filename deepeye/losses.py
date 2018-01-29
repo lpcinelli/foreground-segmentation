1276ae739f68d533620489a172b37945139fe988
lpcinelli <lpcinelli@gmail.com>
2018-01-29 16:41:56 -0200

    Bugfix in roi and rename MaskedDiceLoss attrtibute
    Substitute buggy one-liner for proper if None test
    Rename th attribute in MaskedDiceLoss to threshold

--- deepeye/losses.py
+++ deepeye/losses.py
@@ -5,25 +5,28 @@ from torch.nn.modules.module import Module
 
 class MaskedBinaryCrossEntropy(Module):
     def forward(self, input, target, roi=None):
-        roi = roi or torch.ones_like(input)
-        return F.binary_cross_entropy(
-               torch.sigmoid(input), target, size_average=False) * roi).sum()/roi.sum()
+        if roi is None:
+            roi = torch.ones_like(input)
+        return (F.binary_cross_entropy(
+            torch.sigmoid(input), target, reduce=False) * roi).sum() / roi.sum()
 
 
 bce = MaskedBinaryCrossEntropy
 
 
 class MaskedDiceLoss(Module):
-    def __init__(self, th= 0.5, smooth=1, eps=1e-12):
+    def __init__(self, threshold=0.5, smooth=1, eps=1e-12):
         super().__init__()
-        self.th = th
+        self.threshold = threshold
         self.eps = eps
         self.smooth = smooth
 
     def forward(self, input, target, roi=None):
-        roi = roi or torch.ones_like(input)
-        input = (torch.sigmoid(input) > self.th).float()
+        if roi is None:
+            roi = torch.ones_like(input)
+        input = (torch.sigmoid(input) > self.threshold).float()
         intersect = (input * target * roi).view(input.size(0), -1).sum(1)
         union = ((input + target) * roi).view(input.size(0), -1).sum(1)
 
-        return (1 - (2 * intersect + self.smooth) / ((union + self.eps)).mean() + self.smooth)
+        return (1 - (2 * intersect + self.smooth) /
+                ((union + self.eps)).mean() + self.smooth)

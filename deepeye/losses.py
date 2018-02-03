323a082801a8adaa5c33670a3bc13f1d1253f56c
Lucas Pinheiro Cinelli <lucas.cinelli@teresopolis.smt.ufrj.br>
2018-02-02 22:48:26 -0200

    Fix dice loss and define new soft dice loss
    Renames previous MaskedDiceLoss to MaskedHardDiceLoss, fixes bug in
    computation, adds option (per batch/per image), adds docstring.
    Defines new MaskedSoftDiceLoss class (same as MaskedHardDiceLoss but
    without threshold function)

--- deepeye/losses.py
+++ deepeye/losses.py
@@ -10,16 +10,33 @@ class MaskedBinaryCrossEntropy(Module):
         return (F.binary_cross_entropy(
             torch.sigmoid(input), target, reduce=False) * roi).sum() / roi.sum()
 
-
 bce = MaskedBinaryCrossEntropy
 
 
-class MaskedDiceLoss(Module):
-    def __init__(self, threshold=0.5, smooth=1, eps=1e-12):
+class MaskedHardDiceLoss(Module):
+    """Non-differentiable (due to threshold operation on input arg)
+       Sørensen–Dice coefficient with mask for comparing the similarity in
+       region of interest of two batch of data.
+       Coefficient ranges between 0 to 1, 1 if totally matchs.
+       Inputs:
+                threshold (float): threshold value above which is 1, else 0
+                eps (float): small value to avoid division by zero
+                smooth (float): negative value added to both num. and den.
+                                (laplacian/additive smoothing)
+                per_img (bool): If True, computes loss per image and then
+                         averages, else computes it per batch
+       Shape:
+                input (tensor): `(N, *)` where `*` means, any number of
+                       additional dimensions
+                target (tensor): `(N, *)`, same shape as the input
+                roi (tensor): `(N, *)`, same shape as the input
+    """
+    def __init__(self, threshold=0.5, smooth=1, eps=1e-12, per_img=False):
         super().__init__()
         self.threshold = threshold
         self.eps = eps
         self.smooth = smooth
+        self.per_img = per_img
 
     def forward(self, input, target, roi=None):
         if roi is None:
@@ -28,5 +45,50 @@ class MaskedDiceLoss(Module):
         intersect = (input * target * roi).view(input.size(0), -1).sum(1)
         union = ((input + target) * roi).view(input.size(0), -1).sum(1)
 
-        return (1 - (2 * intersect + self.smooth) /
-                ((union + self.eps)).mean() + self.smooth)
+        if self.per_img is True:
+            return (1 - (2 * intersect + self.smooth) /
+                    ((union + self.eps) + self.smooth)).mean()
+
+        return 1 - ((2 * intersect.sum() + self.smooth) /
+                    (union.sum() + self.eps + self.smooth))
+
+harddice = MaskedHardDiceLoss
+
+
+class MaskedSoftDiceLoss(Module):
+    """Non-differentiable Sørensen–Dice coefficient for comparing the
+       similarity in the region of interest of two batch of data.
+       Coefficient ranges between 0 to 1, 1 if totally matchs.
+       Inputs:
+                eps (float): small value to avoid division by zero
+                smooth (float): negative value added to both num. and den.
+                                (laplacian/additive smoothing)
+                per_img (bool): If True, computes loss per image and then
+                         averages, else computes it per batch
+       Shape:
+                input (tensor): `(N, *)` where `*` means, any number of
+                       additional dimensions
+                target (tensor): `(N, *)`, same shape as the input
+                roi (tensor): `(N, *)`, same shape as the input
+    """
+    def __init__(self, threshold=0.5, smooth=1, eps=1e-12, per_img=False):
+        super().__init__()
+        self.threshold = threshold
+        self.eps = eps
+        self.smooth = smooth
+        self.per_img = per_img
+
+    def forward(self, input, target, roi=None):
+        if roi is None:
+            roi = torch.ones_like(input)
+        intersect = (input * target * roi).view(input.size(0), -1).sum(1)
+        union = ((input + target) * roi).view(input.size(0), -1).sum(1)
+
+        if self.per_img is True:
+            return (1 - (2 * intersect + self.smooth) /
+                    ((union + self.eps) + self.smooth)).mean()
+
+        return 1 - ((2 * intersect.sum() + self.smooth) /
+                    (union.sum() + self.eps + self.smooth))
+
+softdice = MaskedSoftDiceLoss

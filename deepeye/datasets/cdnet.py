610b8b087e13a22d0f2a3c141c87d0e51f737bce
Igor Macedo Quintanilha <igor.quintanilha@poli.ufrj.br>
2018-03-07 06:13:19 -0300

    Add option of retrieving the data by name in CDNetDataset Code refactor in CDNetDataset Improve docstring

--- deepeye/datasets/cdnet.py
+++ deepeye/datasets/cdnet.py
@@ -18,28 +18,36 @@ from .file import DataFile
 
 DEFAULT_SHAPE = (2, 192, 256)
 
-class CDNetDataset(data.Dataset):
 
-    def __init__(self, manifest_path, database_path, transform=None, **kwargs):
+class CDNetDataset(data.Dataset):
+    def __init__(self,
+                 manifest_path,
+                 database_path,
+                 transform=None,
+                 training=False,
+                 shrink_data=False,
+                 input_shape=DEFAULT_SHAPE,
+                 augmentation=False):
         '''
         Inits an ImageFile instance.
 
-        @param csv_file csv file path containing the targets.
-        @param transform Images transforms.
-        @param target_transform Labels transform.
-        @param loader Images loader.
+        Args:
+            csv_file: csv file path containing the targets.
+            transform: Images transforms.
+            target_transform: Labels transform.
+            loader: Images loader.
         '''
 
         # Loads data
         self.database_path = database_path
-        training = kwargs.get('training', False)
-        shrink_data = kwargs.pop('shrink_data', False)
-        data = self.from_file(manifest_path, training, shrink_data)
+        training = training
+        shrink_data = shrink_data
+        data, self.names = self.from_file(manifest_path, training, shrink_data)
 
         if len(data) == 0:
-            raise(RuntimeError("Found 0 images in path: " + manifest_path + "\n" +
-                               "Supported image extensions are: " +
-                               ",".join(IMG_EXTENSIONS)))
+            raise (RuntimeError(
+                "Found 0 images in path: " + manifest_path + "\n" +
+                "Supported image extensions are: " + ",".join(IMG_EXTENSIONS)))
 
         # Main data
         self.data = data
@@ -47,9 +55,9 @@ class CDNetDataset(data.Dataset):
         # Saving data
         self.manifest_path = manifest_path
 
-        self.input_shape = kwargs.pop('input_shape', DEFAULT_SHAPE)
+        self.input_shape = input_shape
         if not transform:
-            transform = transforms(self.input_shape, **kwargs)
+            transform = transforms(self.input_shape, training, augmentation)
         self.transform = transform
 
     def __getitem__(self, index):
@@ -61,8 +69,20 @@ class CDNetDataset(data.Dataset):
         @return The image and its respective target.
         '''
 
+        if isinstance(index, str):
+            idxs = [
+                idx for idx, name in enumerate(self.names) if index in name
+            ]
+            if len(idxs) == 0:
+                raise KeyError('Value not found')
+            if len(idxs) > 1:
+                raise KeyError('Non-unique key')
+
+            data = self.data[idxs[0]]
+        else:
+            data = self.data[index]
         # Get single data
-        (input_, bg_model), (target, roi) = self.data[index]
+        (input_, bg_model), (target, roi) = data
 
         input_ = self.loader(input_)
         bg_model = self.loader(bg_model)
@@ -71,8 +91,8 @@ class CDNetDataset(data.Dataset):
 
         # Transforming image
         if self.transform is not None:
-            input_, target, roi = self.transform((input_, bg_model, target, roi))
-
+            input_, target, roi = self.transform((input_, bg_model, target,
+                                                  roi))
         # Return
         return input_, target, roi
 
@@ -95,7 +115,7 @@ class CDNetDataset(data.Dataset):
     def from_file(self, csv_file, training, shrink_data):
         # Opening file
         dataset = pd.read_csv(csv_file)
-        imgs, targets = [], []
+        imgs, targets, names = [], [], []
 
         for _, row in dataset.iterrows():
             if training and shrink_data and row['negative_only']:
@@ -117,11 +137,12 @@ class CDNetDataset(data.Dataset):
 
             imgs.append((input_path, bg_path))
             targets.append((target_path, roi_path))
+            names.append(input_path)
 
-        return list(zip(imgs, targets))
+        return list(zip(imgs, targets)), names
 
 
-def transforms(input_shape,training=False, augmentation=False):
+def transforms(input_shape, training=False, augmentation=False):
 
     if not training and augmentation is not False:
         raise ValueError('Combinations of parameters not permitted. '
@@ -129,12 +150,17 @@ def transforms(input_shape,training=False, augmentation=False):
 
     C, H, W = input_shape
 
-    compose = [Resize((H, W), INTERPOLATION['BILINEAR']), MergeChannels(),
-               ToTensor(), BinarizeTarget()]
+    compose = [
+        Resize((H, W), INTERPOLATION['BILINEAR']),
+        MergeChannels(),
+        ToTensor(),
+        RoiCrop(),
+        BinarizeTarget()
+    ]
 
     if C < 3:
         compose = [Grayscale(1)] + compose
     if augmentation is True:
-            compose = [RandomHorizontalFlip()] + compose
+        compose = [RandomHorizontalFlip()] + compose
 
     return vision_transforms.Compose(compose)
